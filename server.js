ad426ba590358b8083cdf10124379568914fce1d
Mohith Sarvepalli <61641905+SaiSnehithaRavuru@users.noreply.github.com>
2025-02-09 22:37:17 +0530

    Loading data from JSON File

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
 const express=require('express');
+const fs=require('fs');
 const app=express();
 const port=3000;
 
 app.use('/',express.static('public'));
 
-
-const budget = {
-    myBudget:[
-    {
-        title: 'Eat Out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-]
-};
+    
 
 app.get('/hello',(req,res)=>{
     res.send('Hello World!');
 });
 
 app.get('/budget',(req,res)=>{
-    res.json(budget);
+    fs.readFile("budget-data.json", "utf8", (err, budget) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }else{
+            res.json(JSON.parse(budget));
+        }
+      });
 });
 
 app.listen(port,()=>{
